f0b20e5be7173b3529e8377da56aec965163a9e2
paccy-tech <pacifiquetwagirayesu@gmail.com>
2020-04-29 18:44:43 +0800

    initial commit

---
+++ src/ChatRoom.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class ChatRoom {
+    private int IdChatRoom;
+    private String description;
+    private Map<String, Integer>chats;
+
+    public ChatRoom(int idChatRoom, String description) {
+        IdChatRoom = idChatRoom;
+        this.description = description;
+        this.chats = new HashMap<>();
+        this.chats.put("6",6);
+        this.chats.put("1",1);
+        this.chats.put("2",2);
+        this.chats.put("3",3);
+        this.chats.put("4",6);
+
+
+
+
+    }
+
+    public int getIdChatRoom() {
+        return IdChatRoom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getChats() {
+        return chats;
+    }
+
+    public void ChangeLocation(String direction, int location){
+        chats.put(direction,location);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,72 @@
+
+import java.util.*;
+
 public class Main {
 
+    private static Map<Integer, ChatRoom> chatRoomMap = new HashMap<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello, guys");
+        Scanner user = new Scanner(System.in);
+        chatRoomMap.put(0, new ChatRoom(0, "Welcome!, How can help you? \n ENROLLMENT?,SCHOOL FEES......"));
+        chatRoomMap.put(1, new ChatRoom(1, "In which course?, we have COMPUTER Engineering, ELECTRONICS Engineering, and CIVIL Engineering"));
+
+        chatRoomMap.put(2, new ChatRoom(2, "You can leave your email, we will back back to you in an hour via email."));
+        chatRoomMap.put(3, new ChatRoom(3, " do you have another question?"));
+
+        chatRoomMap.put(4, new ChatRoom(4, "28/05/2020"));
+        chatRoomMap.put(5, new ChatRoom(5, "it's okay?"));
+
+        chatRoomMap.put(6, new ChatRoom(6, "Thanks for being with us! we will contact you via email"));
+
+        chatRoomMap.get(1).ChangeLocation("1", 1);
+
+        chatRoomMap.get(2).ChangeLocation("2",2);
+        chatRoomMap.get(3).ChangeLocation("2",3);
+        chatRoomMap.get(4).ChangeLocation("2",4);
+        chatRoomMap.get(5).ChangeLocation("2",5);
+        chatRoomMap.get(6).ChangeLocation("2",6);
+
+
+
+        Map<String, String> vocabulary = new HashMap<>();
+        vocabulary.put("QUIT","6");
+        vocabulary.put("ENROLLMENT","1");
+        vocabulary.put("COMPUTER","2");
+        vocabulary.put("EMAIL","3");
+        vocabulary.put("NO","6");
+
+
+        int loc = 0;
+        while (true){
+            System.out.println(chatRoomMap.get(loc).getDescription());
+            if (loc ==6){
+                break;
+            }
+            Map<String, Integer>exits = chatRoomMap.get(loc).getChats();
+
+            System.out.println();
+
+            String direction = user.nextLine().toUpperCase();
+            if (direction.length()>1) {
+                String[] words = direction.split(" ");
+                for (String word : words) {
+                    if (vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
+            if (exits.containsKey(direction)){
+                loc = exits.get(direction);
+            }else{
+                System.out.println("You can not go in that direction");
+            }
+        }
+
     }
 }
+
+
+
+
+
